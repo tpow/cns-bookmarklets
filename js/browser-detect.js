f6b71d66c26c9d2607e7cccf41fdc91221d6b4aa
Tim Powell <timbzr@worldtimzone.com>
2020-08-18 08:18:16 -0400

    Minimal desktop os detection

--- js/browser-detect.js
+++ js/browser-detect.js
@@ -12,14 +12,16 @@ function getBrowser(a){
 		var ios = /iphone|ipad|ipod/.test(e);
 		var android = /android/.test(e);
 		var t=/(edg)e?[ \/]([\w.]+)/.exec(e)||/(chrome)[ \/]([\w.]+)/.exec(e)||/(webkit)[ \/]([\w.]+)/.exec(e)||/(opera)(?:.*version|)[ \/]([\w.]+)/.exec(e)||/(msie) ([\w.]+)/.exec(e)||/(firefox)[ \/]([\w.]+)/.exec(e)||/(trident)(?:.*? rv:([\w.]+)|)/.exec(e)||e.indexOf("compatible")<0&&/(mozilla)(?:.*? rv:([\w.]+)|)/.exec(e)||[];
+		var os=/windows/.test(e)?"win":/mac/.test(e)?"mac":/linux/.test(e)?"linux":/x11/.test(e)?"nix":"";
 		return{
 			"browser":t[1]||"",
 			"version":t[2]||"0",
+			"os":os,
 			"ios":ios||"",
 			"android":android||"",
 		}
 	};
 	var n=t(a),i={},e={};
-	n.browser && (i[n.browser]=!0, i.version=n.version, i.ios=n.ios, i.android=n.android), i.chrome ? i.webkit=!0 : i.webkit && (i.safari=!0), i.edg ? i.edge=!0 && (delete i.edg) : null, "73"<i.version && i.edge ? (i.chrome=!0) : null, i.trident ? !(i.edge) && (i.msie=!0) : null, i.android ? i.safari=!1 : null, e.browser=i;
+	n.browser && (i[n.browser]=!0, i.version=n.version, i.os=n.os, i.ios=n.ios, i.android=n.android), i.chrome ? i.webkit=!0 : i.webkit && (i.safari=!0), i.edg ? i.edge=!0 && (delete i.edg) : null, "73"<i.version && i.edge ? (i.chrome=!0) : null, i.trident ? !(i.edge) && (i.msie=!0) : null, i.android ? i.safari=!1 : null, e.browser=i;
 	return e;
 }
